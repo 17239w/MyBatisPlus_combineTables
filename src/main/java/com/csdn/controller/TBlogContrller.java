aa09b25eff2871e02f41bd56a60e0673e344ca0d
17239w <w17239@outlook.com>
2023-03-18 20:22:10 +0800

    MyBatisPlus多表查询

--- src/main/java/com/csdn/controller/TBlogContrller.java
+++ src/main/java/com/csdn/controller/TBlogContrller.java
@@ -1,13 +1,28 @@
 package com.csdn.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.csdn.domain.TBlog;
+import com.csdn.service.TBlogService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/tBlog")
-public class TBContrller {
+public class TBlogContrller {
 
-    @Autowired
-    private TBService tbService;
+    @Resource
+    private TBlogService tBlogService;
+
+    @GetMapping("/staticQuery")
+    public String staticQuery(){
+        return tBlogService.staticQuery();
+    }
+
+    @GetMapping("/dynamicPageQuery")
+    public String dynamicPageQuery(){
+        return tBlogService.dynamicPageQuery(null, null, null).toString();
+    }
 }
